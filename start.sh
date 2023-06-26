3820f0a3841d68c358679c2a18a75aa4370ccdd2
ethanlee928 <ethan2000.el@gmail.com>
2023-06-26 10:43:52 +0800

    revert name change in Dockerfile

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@ image_name="jetson-inference:latest"
 
 if [[ "$(docker images -q ${image_name} 2> /dev/null)" == "" ]];
 then
-    docker build -t ${image_name} .
+    docker build -t ${image_name} -f Dockerfile.custom .
 fi
 
 ./docker/run.sh -c ${image_name}
