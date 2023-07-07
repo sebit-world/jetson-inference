5935063bf542bced08f38b8e276af69a104957e0
ethanlee928 <ethan2000.el@gmail.com>
2023-07-07 16:55:47 +0800

    refactor

--- data/codes/pyfirmata/detectnet_led.py
+++ data/codes/pyfirmata/detectnet_led.py
@@ -6,32 +6,16 @@ import sys
 
 from base import Board, LED
 
-# parse the command line
-parser = argparse.ArgumentParser(
-    description="Locate objects in a live camera stream using an object detection DNN."
-)
+PERSON_CLASS_ID = 1
 
-parser.add_argument(
-    "input_URI", type=str, default="", nargs="?", help="URI of the input stream"
-)
-parser.add_argument(
-    "output_URI", type=str, default="", nargs="?", help="URI of the output stream"
-)
-parser.add_argument(
-    "--network",
-    type=str,
-    default="ssd-mobilenet-v2",
-    help="pre-trained model to load (see below for options)",
-)
-parser.add_argument(
-    "--overlay",
-    type=str,
-    default="box,labels,conf",
-    help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'",
-)
-parser.add_argument(
-    "--threshold", type=float, default=0.5, help="minimum detection threshold to use"
-)
+# parse the command line
+parser = argparse.ArgumentParser(description="Locate objects in a live camera stream using an object detection DNN.")
+
+parser.add_argument("input_URI", type=str, default="", nargs='?', help="URI of the input stream")
+parser.add_argument("output_URI", type=str, default="", nargs='?', help="URI of the output stream")
+parser.add_argument("--network", type=str, default="ssd-mobilenet-v2", help="pre-trained model to load (see below for options)")
+parser.add_argument("--overlay", type=str, default="box,labels,conf", help="detection overlay flags (e.g. --overlay=box,labels,conf)\nvalid combinations are:  'box', 'labels', 'conf', 'none'")
+parser.add_argument("--threshold", type=float, default=0.5, help="minimum detection threshold to use") 
 
 try:
     opt = parser.parse_known_args()[0]
@@ -48,9 +32,9 @@ input = jetson.utils.videoSource(opt.input_URI, argv=sys.argv)
 output = jetson.utils.videoOutput(opt.output_URI, argv=sys.argv)
 
 
-# process frames until the user exits
 with Board("/dev/ttyUSB0", True) as board:
     led = LED(board, 6)
+    # process frames until the user exits
     while True:
         # capture the next image
         img = input.Capture()
@@ -58,13 +42,10 @@ with Board("/dev/ttyUSB0", True) as board:
         # detect objects in the image (with overlay)
         detections = net.Detect(img, overlay=opt.overlay)
 
-        count = 0
-        for detection in detections:
-            if detection.ClassID == 1:
-                count += 1
-
         # render the image
         output.Render(img)
+
+        count = sum(d.ClassID == PERSON_CLASS_ID for d in detections)
         if count >= 1:
             led.on()
         else:
